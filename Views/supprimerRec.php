b94480d44fe7445977e7258a570de6908e398db9
relo3 <74118821+relo3@users.noreply.github.com>
2022-05-12 12:19:07 +0100

    Delete Views directory

--- Views/supprimerRec.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include '../Controller/reclamationC.php';
-	$reclamationController=new reclamationC();
-	$reclamationController->supprimerticket($_POST["Numtickets"]);
-	header('Location:afficherRec.php');
-?>
